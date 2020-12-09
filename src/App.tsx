bf6d6d84cf900d686812d848cfd82ca3b8986c1f
Franco Dalmasso <francodalmasso@gmail.com>
2020-12-09 20:21:02 -0300

    Add formatting on save config for VSCode

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import React, { useState, useEffect } from 'react';
-import { ThemeProvider, jsx, Container, Card, Button, Heading } from "theme-ui";
-import deepTheme from "./theme";
+import { ThemeProvider, jsx, Container, Card, Button, Heading } from 'theme-ui';
+import deepTheme from './theme';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,11 +9,18 @@ function App() {
     <ThemeProvider theme={deepTheme}>
       <Container
         p={4}
-        bg='muted'
+        bg="muted"
         sx={{
-          height: "100vh",
-        }}>
-        <Card sx={{ background: "rgba(255, 255, 255, 0.3)", borderRadius: "1rem", p: 3 }}>
+          height: '100vh',
+        }}
+      >
+        <Card
+          sx={{
+            background: 'rgba(255, 255, 255, 0.3)',
+            borderRadius: '1rem',
+            p: 3,
+          }}
+        >
           <Heading sx={{ marginBottom: 4 }}>Simple counter</Heading>
           <Button variant="primary" onClick={() => setCount(count + 1)}>
             +1
